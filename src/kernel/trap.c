2745ba054a0453a90b98103c94e0bea602ff425e
MathxH Chen <brainfvck@foxmail.com>
2020-08-02 12:34:56 +0800

    update

--- src/kernel/trap.c
+++ src/kernel/trap.c
@@ -296,26 +296,26 @@ void do_virtualization_exception(unsigned long rsp,unsigned long error_code)
 void sys_vector_handler_init()
 {
     set_trap_gate(0,1,divide_error);
-	set_trap_gate(1,1,debug);
-	set_interrupt_gate(2,1,nmi);
-	set_system_gate(3,1,int3);
-	set_system_gate(4,1,overflow);
-	set_system_gate(5,1,bounds);
-	set_trap_gate(6,1,undefined_opcode);
-	set_trap_gate(7,1,dev_not_available);
-	set_trap_gate(8,1,double_fault);
-	set_trap_gate(9,1,coprocessor_segment_overrun);
-	set_trap_gate(10,1,invalid_TSS);
-	set_trap_gate(11,1,segment_not_present);
-	set_trap_gate(12,1,stack_segment_fault);
-	set_trap_gate(13,1,general_protection);
-	set_trap_gate(14,1,page_fault);
-	//15 Intel reserved. Do not use.
-	set_trap_gate(16,1,x87_FPU_error);
-	set_trap_gate(17,1,alignment_check);
-	set_trap_gate(18,1,machine_check);
-	set_trap_gate(19,1,SIMD_exception);
-	set_trap_gate(20,1,virtualization_exception);
+	// set_trap_gate(1,1,debug);
+	// set_interrupt_gate(2,1,nmi);
+	// set_system_gate(3,1,int3);
+	// set_system_gate(4,1,overflow);
+	// set_system_gate(5,1,bounds);
+	// set_trap_gate(6,1,undefined_opcode);
+	// set_trap_gate(7,1,dev_not_available);
+	// set_trap_gate(8,1,double_fault);
+	// set_trap_gate(9,1,coprocessor_segment_overrun);
+	// set_trap_gate(10,1,invalid_TSS);
+	// set_trap_gate(11,1,segment_not_present);
+	// set_trap_gate(12,1,stack_segment_fault);
+	// set_trap_gate(13,1,general_protection);
+	// set_trap_gate(14,1,page_fault);
+	// //15 Intel reserved. Do not use.
+	// set_trap_gate(16,1,x87_FPU_error);
+	// set_trap_gate(17,1,alignment_check);
+	// set_trap_gate(18,1,machine_check);
+	// set_trap_gate(19,1,SIMD_exception);
+	// set_trap_gate(20,1,virtualization_exception);
 
 	//set_system_gate(SYSTEM_CALL_VECTOR,7,system_call);
 }
